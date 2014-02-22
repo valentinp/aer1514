1b4c2312caa6675f6a27fc6d90efeb5876fc9858
leeclement <clement.leopold@gmail.com>
2014-02-22 17:51:16 -0500

    Updated terrain assessment

--- displayKinectDepth.m
+++ displayKinectDepth.m
@@ -1,14 +1,14 @@
-function displayKinectDepth(depth, figNum)
+function displayKinectDepth(depth, figHandle)
 % Displays depth information captured from Kinect
 
     % Initialise FIGURE
-    width = 640; 
-    height = 480;
-    % depth image
-    figure(figNum);
-    h = imagesc(zeros(height,width,'uint16'));
+%     width = 640; 
+%     height = 480;
+%     % depth image
+%     figure(figNum);
+%     h = imagesc(zeros(height,width,'uint16'));
     
     % Update figure 
-    set(h,'CData',flipdim(depth,2)); 
+    set(figHandle,'CData',flipdim(depth,2)); 
     drawnow;
 end
