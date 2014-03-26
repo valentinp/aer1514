d3ffae926ba671916a91a2a8d8950b453cd3eb04
Valentin Peretroukhin <valentinp@gmail.com>
2014-03-26 18:32:23 -0400

    Added new convert functions for kinect data

---
+++ kinect_calib/mxNiConvertProjectiveToRealWorld.m
@@ -0,0 +1,7 @@
+function [realWorld] = mxNiConvertProjectiveToRealWorld( depth )
+%MXNICONVERTPROJECTIVETOREALWORLD Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ kinect_calib/mxNiConvertRealWorldToProjective.m
@@ -0,0 +1,6 @@
+function [depth] = mxNiConvertRealWorldToProjective( realWorld )
+%MXNICONVERTPROJECTIVETOREALWORLD Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
