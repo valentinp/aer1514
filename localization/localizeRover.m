eeba7e79974e809ff8d1775e4f2c5f2e9173a9db
Valentin Peretroukhin <valentinp@gmail.com>
2014-03-28 12:23:40 -0400

    Simplified tracking

--- localization/localizeRover.m
+++ localization/localizeRover.m
@@ -24,12 +24,6 @@ hsv = rgb2hsv(rgb);
  red_region = hsv(:,:,1) >= red_h_rng(1) & hsv(:,:,1) <= red_h_rng(2) & hsv(:,:,2) > red_s_min & hsv(:,:,3) > red_v_min;
  blue_region = hsv(:,:,1) >= blue_h_rng(1) & hsv(:,:,1) <= blue_h_rng(2)& hsv(:,:,2) > blue_s_min & hsv(:,:,3) > blue_v_min;
 
- 
-
-kinectPoints_k = mxNiConvertProjectiveToRealWorld(context, depth) / 1000;  % height x width x 3 (meters)
-
-%  kinectPoints_k = mxNiDepthRealWorld(context) / 1000;
-%  kinectPoints_k=permute(kinectPoints_k,[2 1 3]);
 
 if sum(red_region) < 2
    disp('WARNING: No red pixels found'); 
@@ -45,34 +39,25 @@ end
 
 
 % Use RANSAC to find the best centroid for the red and blue spheres
-K = 100;
+K = 200;
 thresh = 5;       % inlier error threshold (radius meas. in pixels)
 %maxSearchIterations = 30; % max amount of times to search for radii that are close together
 maxInliersB = 0;
 maxInliersR = 0;
 
     for k = 1:K
-        % Set centroid
-        
         %Ensure centroids are within 40 cm of each other
         sampleIndB = randi(size(blue_r,1));
         testBlueCentroid = [blue_c(sampleIndB); blue_r(sampleIndB)];
         sampleIndR = randi(size(red_r,1));
         testRedCentroid = [red_c(sampleIndR); red_r(sampleIndR)];
-        redVec_k = kinectPoints_k(testRedCentroid(2), testRedCentroid(1), :);
-        blueVec_k = kinectPoints_k(testBlueCentroid(2), testBlueCentroid(1), :);
-        redVec_k = redVec_k(:);
-        blueVec_k = blueVec_k(:);
-        lateral_vec = redVec_k - blueVec_k;
-        ball_sep = norm(lateral_vec);
-        
-             if ~isnan(prevPixVec)
-                cosChange = normalize(prevPixVec)'*normalize(testRedCentroid - testBlueCentroid);
-            else
-                cosChange = 1;
-            end
-        
-            if  norm(testRedCentroid - testBlueCentroid) > 350  %&& cosChange > 0.866 %Less than 30 degree change
+%             if ~isnan(prevPixVec)
+%                 cosChange = normalize(prevPixVec)'*normalize(testRedCentroid - testBlueCentroid);
+%             else
+%                 cosChange = 1;
+%             end
+%         
+            if  norm(testRedCentroid - testBlueCentroid) > 350  %Less than 30 degree change
                 continue;
             end
         errB = (blue_c - testBlueCentroid(1)).^2 + (blue_r - testBlueCentroid(2)).^2;
@@ -94,12 +79,18 @@ maxInliersR = 0;
     end
 
 if ~isnan(bestRedCentroid(1)) && ~isnan(bestBlueCentroid(1))
-bestRedVec_k = kinectPoints_k(bestRedCentroid(2), bestRedCentroid(1), :);
-bestBlueVec_k = kinectPoints_k(bestBlueCentroid(2), bestBlueCentroid(1), :);
-bestRedVec_k = reshape(bestRedVec_k, [3 1]);
-bestBlueVec_k = reshape(bestBlueVec_k, [3 1]);
-% else
-%     disp('WARNING: Could not find a small enough ball separation.'); 
+    kinectPoints_k = mxNiConvertProjectiveToRealWorld(context, depth) / 1000;  % height x width x 3 (meters)
+    bestRedVec_k = kinectPoints_k(bestRedCentroid(2), bestRedCentroid(1), :);
+    bestBlueVec_k = kinectPoints_k(bestBlueCentroid(2), bestBlueCentroid(1), :);
+    bestRedVec_k = reshape(bestRedVec_k, [3 1]);
+    bestBlueVec_k = reshape(bestBlueVec_k, [3 1]);
+    ballSep = norm(bestRedVec_k - bestBlueVec_k);
+    if ballSep < 0.3 || ballSep > 0.6
+        bestRedCentroid = NaN;
+        bestBlueCentroid = NaN;
+        bestRedVec_k = NaN;
+        bestBlueVec_k = NaN;
+    end
 end
 
 end
