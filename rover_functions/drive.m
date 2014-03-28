d55994435853050953e48d3f96f7ab4d65ebcf2f
leeclement <clement.leopold@gmail.com>
2014-03-28 14:20:56 -0400

    Bug fixes, safety parameters for drive()

--- rover_functions/drive.m
+++ rover_functions/drive.m
@@ -1,4 +1,11 @@
 function drive( v, omega )
+   vMax = 0.6;
+   omegaAbsMax = 4;
+
+   % Safety first!
+   v = min(v,vMax);
+   omega = sign(omega)*min(abs(omega),omegaAbsMax);
+
    set_param('robulink/v','Value',num2str(v));
    set_param('robulink/omega','Value',num2str(omega));
 end
