ab86da6af57f44ef7564ddd889e9bbdedb9d09df
leeclement <clement.leopold@gmail.com>
2014-03-31 17:31:04 -0400

    Added emergency stop to keyboard control (press 'e')

---
+++ gui/KeyPressFcn.m
@@ -0,0 +1,32 @@
+function KeyPressFcn(src, event)
+    global enableTeleopMode;
+    global v_teleop; global omega_teleop;
+    
+    if enableTeleopMode
+        v = str2double(get_param('robulink/v','Value'));
+        omega = str2double(get_param('robulink/omega', 'Value'));
+        switch(event.Key)
+            case 'leftarrow'
+                omega = omega_teleop;
+%                 disp('left arrow pressed')
+
+            case 'rightarrow'
+                omega = -omega_teleop;
+%                 disp('right arrow pressed')
+
+            case 'uparrow'
+                v = v_teleop;
+%                 disp('up arrow pressed')
+
+            case 'downarrow'
+                v = -v_teleop;
+%                 disp('down arrow pressed')
+        end
+        drive(v,omega)
+    else
+        switch(event.Key)
+            case 'e'
+                hammerheadGUI('btn_EmergStop_Callback'l);
+        end
+    end
+end
---
+++ gui/KeyReleaseFcn.m
@@ -0,0 +1,26 @@
+function KeyReleaseFcn(src, event)
+    global enableTeleopMode;
+    
+    if enableTeleopMode
+        v = str2double(get_param('robulink/v','Value'));
+        omega = str2double(get_param('robulink/omega', 'Value'));
+        switch(event.Key)
+            case 'leftarrow'
+                omega = 0;
+%                 disp('left arrow released')
+
+            case 'rightarrow'
+                omega = 0;
+%                 disp('right arrow released')
+
+            case 'uparrow'
+                v = 0;
+%                 disp('up arrow released')
+
+            case 'downarrow'
+                v = 0;
+%                 disp('down arrow released')
+        end
+        drive(v,omega);
+    end
+end
