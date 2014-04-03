a4f174a0a15992fe65ff4b5b447827760c63a29a
leeclement <clement.leopold@gmail.com>
2014-04-03 18:37:57 -0400

    Updated point cloud alignment to output transformation matrix instead of rotation and translation

--- localization/scalarWeightedPointCloudAlignment.m
+++ localization/scalarWeightedPointCloudAlignment.m
@@ -1,23 +1,28 @@
-function [rotMat, transVec] = scalarWeightedPointCloudAlignment(oldPts, newPts, weights)
+function T_ba = scalarWeightedPointCloudAlignment(pts_a, pts_b)
+% Taken from AER 1514 Lecture 15, slide 20
+% pts_a and pts_b must be a matrix of column vectors
 
+    weights = ones(size(pts_a,2));
     weights = weights ./ sum(weights);
     weights = repmat(weights, [3,1]);
 
-    oldCentroid = sum(weights .* oldPts, 2);
-    newCentroid = sum(weights .* newPts, 2);
+    centroid_a = sum(weights .* pts_a, 2);
+    centroid_b = sum(weights .* pts_b, 2);
     
-    for j = size(oldPts,2) : -1: 1
-        W(:,:,j) = weights(:,j) .* (oldPts(:,j) - oldCentroid)*(newPts(:,j) - newCentroid)';
+    for j = size(pts_a,2) : -1: 1
+        W(:,:,j) = weights(:,j) .* (pts_a(:,j) - centroid_a)*(pts_b(:,j) - centroid_b)';
     end
     
     W = sum(W,3);
     
     [V,S,U] = svd(W);
     
-    rotMat = V * [   1,     0,      0   ; ...
+    R_ba = V * [   1,     0,      0   ; ...
                      0,     1,      0   ; ...
                      0,     0, det(U)*det(V)   ] * U';
                  
-    transVec = -rotMat'*newCentroid + oldCentroid;
+    t_ba_a = -R_ba'*centroid_b + centroid_a;
     
+    T_ba = [R_ba, -R_ba*t_ba_a;
+            0,0,0,1];
 end
